2239075d112a59abad75d3e2670ec480f9fae927
Paysant Adrien <adrien.paysant@he-arc.ch>
2021-02-19 11:32:51 +0100

    model+ generic views

---
+++ DemoASPCRUD/Data/MvcBookContext.cs
@@ -0,0 +1,19 @@
+﻿using DemoASPCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DemoASPCRUD.Data
+{
+    
+    public class MvcBookContext : DbContext
+    {
+        public MvcBookContext(DbContextOptions<MvcBookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+
+
+    }
+}
---
+++ DemoASPCRUD/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoASPCRUD.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Editor { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+      
+    }
+}
